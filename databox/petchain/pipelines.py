e5a32173f1153d5418a0132abb3f51fe2a4e9e2a
xuexingdong <306947352@qq.com>
2018-02-06 11:23:56 +0800

    fix

--- databox/petchain/pipelines.py
+++ databox/petchain/pipelines.py
@@ -1,28 +1,9 @@
-import pymongo
-from pymongo import UpdateOne
+from databox.pipelines import MongoPipeline
 
 
-class PetPipeline:
+class PetPipeline(MongoPipeline):
     collection_name = 'pet'
-
-    def __init__(self, mongo_uri, mongo_db):
-        self.mongo_uri = mongo_uri
-        self.mongo_db = mongo_db
-
-    def open_spider(self, spider):
-        self.client = pymongo.MongoClient(self.mongo_uri)
-        self.db = self.client[self.mongo_db]
-
-    def close_spider(self, spider):
-        self.client.close()
 
     def process_item(self, item, spider):
         self.db[self.collection_name].update_one({'id': item['id']}, {'$set': item}, True)
         return item
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        return cls(
-            mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DATABASE')
-        )
