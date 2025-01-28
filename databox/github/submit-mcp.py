cb7f123729ea7d8d33213b92abd956810124507a
adon <xxd.nobug@gmail.com>
2025-01-28 11:57:23 +0800

    fix:

---
+++ databox/github/submit-mcp.py
@@ -0,0 +1,21 @@
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
+from databox.github.github_repo_search_spider import GithubRepoSearchSpider
+from databox.github.github_repo_spider import GithubRepoSpider
+
+
+def run_spiders():
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(GithubRepoSearchSpider, q='mcp-server')
+    process.crawl(GithubRepoSpider,
+                  match_repos=[
+                      'https://github.com/punkpeye/awesome-mcp-servers',
+                      'https://github.com/wong2/awesome-mcp-servers',
+                      'https://github.com/modelcontextprotocol/servers'
+                  ], match_words=['mcp', 'modelcontextprotocol'])
+    process.start()
+
+
+if __name__ == "__main__":
+    run_spiders()
